45da1a55f8dff81a53f16f148e39fdea35cda7a6
Andreas Nesheim <andreas.nesheim@capgemini.com>
2022-04-06 14:13:34 +0200

    Update MainPage.xaml.cs

--- CalculateDistance/MainPage.xaml.cs
+++ CalculateDistance/MainPage.xaml.cs
@@ -13,7 +13,7 @@ namespace CalculateDistance
 
 		private async void OnCounterClicked(object sender, EventArgs e)
 		{
-            var myLocation = await Geolocation.GetLocationAsync();
+            		var myLocation = await Geolocation.GetLocationAsync();
 
 			var otherLocation = new Location(58.9438705, 5.7118685);
 
